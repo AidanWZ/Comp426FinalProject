17c14a5d7aa2ff531baa4e9db445fba30e18a789
Jason Moxley <jasonmoxley@Jasons-MacBook-Pro.local>
2019-12-09 16:09:44 -0500

    updated axios stuff

--- javascript/addClassesNow.js
+++ javascript/addClassesNow.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $("#no").on("click", no);
 });
 function yes() {
-    window.location.assign('http://localhost:3001/html/class-registration-initial/classRegistrationInitial.html');
+    window.location.assign('http://localhost:3001/html/class-registration/classRegistration.html');
 }
 function no() {
     window.location.assign('http://localhost:3001/html/home/home.html');
--- javascript/login.js
+++ javascript/login.js
@@ -1,47 +1,31 @@
-var fs = require('fs');
 
-const pubRoot = new axios.create({
-    baseURL: "http://localhost:3000/public/Portal"
+const loginRoot = new axios.create({
+    baseURL: "http://localhost:3000/account/login"
 });
 $(document).ready(function(){
     $("#loginbutton").on("click", login);
     $("#createbutton").on("click", createAccount);
-    fixShit();
+    //fixShit();
 });
 async function fixShit() {
-        console.log('test');
-        let headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
-        }
-        let teacher = "Ketan+Mayer-Patel";
-        let schoolId = 1232;
-        //let words = document.getElementById('rmpInput').value;
-        let result = await axios({
-            method: 'get',
-            url: "https://www.ratemyprofessors.com/search.jsp?queryoption=HEADER&queryBy=teacherName&schoolName=The+University+of+North+Carolina+at+Chapel+Hill&schoolID=%s&query=%1232Ketan+Mayer-Patel",
-            // headers: headers, 
-        });
-        console.log(result);
-        fs.writeFileSync(process.cwd() + '/class-files/html/rmpTest.html', result, 'utf8');
-    // const result = await pubRoot.get('/Login/' + username);
+    let result = await pubRoot.post('test/', { data: {"testKey": "testing123"}});
 }
-function login() {
+async function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-    if (isValidUser(username, password)) {
-        localStorage.setItem("username", username);
-        document.getElementById("message").innerHTML = '<span class="has-text-success">Success! You are now logged in.</span>';
+    try {
+        let result = await loginRoot.post({
+            "name": username,
+            "pass": password,
+        });
+        let token = result['jwt'];
+        localStorage.setItem("jwt", token);
         window.location.assign('http://localhost:3001/html/home/home.html');
-    }
-    else {
+    } catch (error) {
         $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your email and password and your internet connection.</span>');
-    }  
-}
-function createAccount() {
-    window.location.replace('http://localhost:3001/html/create-user/createUser.html');
+    }
 }
 
-async function isValidUser(username, password) {
-    const serverpassword = await pubRoot.get('/User-data/Login/'+username);
-    return password == serverpassword;
+function createAccount() {
+    window.location.replace('http://localhost:3001/html/create-user/createUser.html');
 }
